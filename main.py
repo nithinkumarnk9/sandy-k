b3d9398ee452aaf9ae29feaf193bbea028425bfd
nithin Kumar <nithinkumarnk999@gmail.com>
2024-09-07 13:54:52 +0530

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "<h1>Nithin Kumar</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
